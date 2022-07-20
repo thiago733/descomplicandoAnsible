01dc6936d34524d42acf203521ab518c93176ca1
thiago733 <thiago733@gmail.com>
2022-07-20 14:48:46 -0400

    adicionando o AWX

--- src/playbooks/instalaAWX/roles/instalandoK3s/tasks/baixaeInstalaCluster.sh
+++ src/playbooks/instalaAWX/roles/instalandoK3s/tasks/baixaeInstalaCluster.sh
@@ -1,27 +1,10 @@
-- name: Instalando o AWX
-  shell: git clone https://github.com/ansible/awx-operator.git && cd awx-operator && \
-         sudo apt install -y make && \
-         git clone https://github.com/ansible/awx-operator.git && cd awx-operator && \
-         export NAMESPACE=awx && \
-         sudo chmod 755 /etc/rancher/k3s/k3s.yaml && \
-         kubectl create ns ${NAMESPACE} && \
-         sudo sudo kubectl config set-context --current --namespace=$NAMESPACE && \
-         RELEASE_TAG=`curl -s https://api.github.com/repos/ansible/awx-operator/releases/latest | grep tag_name | cut -d '"' -f 4` && \
-         echo $RELEASE_TAG && \
-         git checkout $RELEASE_TAG && \
-         export NAMESPACE=awx && \
-         make deploy && \
-         cat <<EOF | kubectl create -f -
-         apiVersion: v1
-         kind: PersistentVolumeClaim
-         metadata:
-           name: static-data-pvc
-           namespafice: awx
-         spec:
-           accessModes:
-             - ReadWriteOnce
-           storageClassName: local-path
-           resources:
-             requests:
-             storage: 5Gi
-           EOF
+sudo apt install -y make && \
+git clone https://github.com/ansible/awx-operator.git && cd awx-operator && \
+export NAMESPACE=awx && \
+sudo chmod 755 /etc/rancher/k3s/k3s.yaml && \
+kubectl create ns ${NAMESPACE} && \
+sudo sudo kubectl config set-context --current --namespace=$NAMESPACE && \
+RELEASE_TAG=`curl -s https://api.github.com/repos/ansible/awx-operator/releases/latest | grep tag_name | cut -d '"' -f 4` && \
+echo $RELEASE_TAG && \
+git checkout $RELEASE_TAG && \
+make deploy
---
+++ src/playbooks/instalaAWX/roles/instalandoK3s/tasks/criaPVC.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cat <<EOF | kubectl create -f -
+---
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: static-data-pvc
+  namespace: awx
+spec:
+  accessModes:
+    - ReadWriteOnce
+  storageClassName: local-path
+  resources:
+    requests:
+      storage: 5Gi
+EOF
