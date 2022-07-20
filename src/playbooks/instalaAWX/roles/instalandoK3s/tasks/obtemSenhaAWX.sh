bf16912d67e20fbf1f85a1b2dbdafd44143310f5
thiago733 <thiago733@gmail.com>
2022-07-20 16:42:15 -0400

    obtendo senha do AWX

---
+++ src/playbooks/instalaAWX/roles/instalandoK3s/tasks/obtemSenhaAWX.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+kubectl get secret awx-admin-password -o go-template='{{range $k,$v := .data}}{{printf "%s: " $k}}{{if not $v}}{{$v}}{{else}}{{$v | base64decode}}{{end}}{{"\n"}}{{end}}' > /tmp/senhaAWX
