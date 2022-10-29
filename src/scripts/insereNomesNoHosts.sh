3980152cdc8872cbe64ad948239db2703e25bdc1
thiago733 <thiago733@gmail.com>
2022-10-29 17:00:00 -0400

    Atualizando Ubuntu

--- src/scripts/insereNomesNoHosts.sh
+++ src/scripts/insereNomesNoHosts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e "192.168.15.121 ansible01\n192.168.15.122 ansible02\n192.168.15.123 ansible03" >> /etc/hosts
+echo -e "192.168.1.121 ansible01\n192.168.1.122 ansible02\n192.168.1.123 ansible03" >> /etc/hosts
